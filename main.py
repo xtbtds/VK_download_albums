f829f9ea72a79e9c4b1363a6784352fbbc2b7722
xtbtds <51954806+xtbtds@users.noreply.github.com>
2023-02-05 17:56:16 +0100

    added multiprocessing

--- main.py
+++ main.py
@@ -1,60 +1,18 @@
 import requests, json
 import random
 import os.path
+from multiprocessing import Pool
+import functools
+from utils import create_folder, download, get_photos, available_albums
 
-"""
-    Use write_json function if you want to store response into .json file:
-    write_json(response.json(), 'file_name.json')
-"""
-
-def write_json(data, filename):
-    with open (filename, 'w') as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
-
-def get_largest(size_dict):
-    if size_dict['width'] >= size_dict['height']:
-        return size_dict['width']
-    else:
-        return size_dict['height']
-
-def create_folder(profile_data):
-    folder_name = "_".join(profile_data)
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name, exist_ok=True)
-    print("Directory {} is created.".format(folder_name))
-    return folder_name
-
-def download(url, save_path):
-    r = requests.get(url, stream=True)
-    try:
-        filename = str(round(random.random(),6))+url.split('/')[-1].split('?')[0]
-    except:
-        print("Can't split filename ", url)
-    completeName = os.path.join(save_path, filename)  
-    with open(completeName, 'bw') as file:
-        for chunk in r.iter_content(4096):
-            file.write(chunk)
-        
-
+#https://oauth.vk.com/authorize?client_id=51526963&redirect_uri=https://oauth.vk.com/blank.html&callback&scope=30&display=mobile&response_type=token&v=5.89
 
 def main():
     token=input("Enter your token: ")
     owner=input("Enter owner id: ")
     
-    # print available albums
-    albums_response = requests.get(f"https://api.vk.com/method/photos.getAlbums", params={
-                                                                        'owner_id': owner,
-                                                                        'need_system': True,
-                                                                        'access_token': token,
-                                                                        'v': 5.89
-                                                                    })
-    
-    albums = albums_response.json()
-    albums = albums['response']['items']
-    for i in range(len(albums)):
-        print(f"{i+1}. {albums[i]['title']}, {albums[i]['size']} photos, id: {albums[i]['id']}")
-
     # select album
+    albums = available_albums(owner, token)
     number = int(input("Choose album number to download: "))
     selected_album_id = int(albums[number-1]['id'])
     album_name = albums[number-1]['title']
@@ -73,26 +31,14 @@ def main():
     save_path = create_folder((first_name, last_name, album_name))
 
     # Downloading photos
-    batches = int(photos_count / 50) + (photos_count % 50 > 0)
-    for i in range(batches):
-        last = (i+1)*50 if photos_count >= (i+1)*50 else i*50 + photos_count % 50
-        print(f"Downloading photos {i*50+1} - {last}...", )
-        photos_response = requests.get(f"https://api.vk.com/method/photos.get", params={
-                                                                            'owner_id': owner, 
-                                                                            'album_id':selected_album_id,
-                                                                            'access_token': token, 
-                                                                            'photo_sizes': True,
-                                                                            'v': 5.89,
-                                                                            'offset': 0 + 50*i
-                                                                        })
+    photos_urls = get_photos(photos_count, owner, selected_album_id, token)
+    print("Download started.")
+    p = Pool()
+    p.map(functools.partial(download, save_path=save_path), photos_urls)
+    p.close()
+    p.join()
+    print(f"{photos_count} photos were downloaded.")
 
-        photos = photos_response.json()
-        photos = photos['response']['items']
-        for photo in photos:
-            sizes = photo['sizes']
-            max_size_url = max(sizes, key=get_largest)['url']
-            download(max_size_url, save_path)
-        print("Done.")
 
 if __name__ == "__main__":
     main()
